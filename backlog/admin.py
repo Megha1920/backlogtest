414648bdb6f85f083b443839c2a47592f7e0ddac
megha.thomas <megha.thomas@innovaturelabs.com>
2025-08-27 10:22:33 +0530

    ignore pycache files and  add models to admin page

--- backlog/admin.py
+++ backlog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Project, TeamMember, Task, TaskStatusHistory
 
+# Custom User Admin
 class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
         ('Role Info', {'fields': ('role', 'google_id')}),
@@ -9,4 +10,26 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'role', 'is_staff')
     list_filter = ('role', 'is_staff', 'is_superuser')
 
+# Project Admin
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manager', 'created_by', 'created_at')
+    list_filter = ('manager',)
+    search_fields = ('name', 'description')
+
+# TeamMember Admin
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('member', 'project')
+    list_filter = ('project',)
+    search_fields = ('member__username', 'project__name')
+
+# Task Admin
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'assigned_to', 'created_by', 'created_at')
+    list_filter = ('status', 'project')
+    search_fields = ('title', 'description')
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(TeamMember, TeamMemberAdmin)
+admin.site.register(Task, TaskAdmin)
